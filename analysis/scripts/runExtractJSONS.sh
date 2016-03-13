ad079c7312079ba8e5839b132669bdb4d0f325f5
Margaret Zientek <mez34@cornell.edu>
2016-03-13 20:00:18 +0100

    added lists for sig & data w/ vtx0

--- analysis/scripts/runExtractJSONS.sh
+++ analysis/scripts/runExtractJSONS.sh
@@ -1,34 +1,52 @@
 #! /bin/sh
 #run extractJSONS.py for all samples in file
 
-# FLASHgg version 1_1_0 w/MET syst for 25ns: 
-python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia.json		-o DoubleEG 		-d lists_25ns_v1_1_0_wMETsys/Data
+#FLASHgg version 1_1_0 w/ Vtx0 for 25ns:
 
-python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o TTGJets  		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o TGJets 		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o WGToLNuG 		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o ZGTo2LG   		-d lists_25ns_v1_1_0_wMETsys/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o DoubleEG 		-d lists_25ns_v1_1_0_wVtx0/Data
 
-python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_dy.json	-o DYJetsToLL		-d lists_25ns_v1_1_0_wMETsys/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP600		-d lists_25ns_v1_1_0_wVtx0/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP800		-d lists_25ns_v1_1_0_wVtx0/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP1000		-d lists_25ns_v1_1_0_wVtx0/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP1200		-d lists_25ns_v1_1_0_wVtx0/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP1400		-d lists_25ns_v1_1_0_wVtx0/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP1700		-d lists_25ns_v1_1_0_wVtx0/MC
+python extractJSONS.py -i datasets/datasets_74X_sig_data_vtx0.json	-o 2HDM_MZP2500		-d lists_25ns_v1_1_0_wVtx0/MC
 
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o ttHJetToGG_M125	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o VBFHToGG_M-125	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o DiPhoton		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o QCD_Pt-30to40	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o QCD_Pt-40toInf	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o QCD_Pt-30toInf	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o GJet_Pt-20to40	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o GJet_Pt-40toInf	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o GluGluHToGG_M-125	-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o VHToGG_M125		-d lists_25ns_v1_1_0_wMETsys/MC
 
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP600		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP800		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1000		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1200		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1400		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1700		-d lists_25ns_v1_1_0_wMETsys/MC
-python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP2500		-d lists_25ns_v1_1_0_wMETsys/MC
+
+
+
+
+
+## FLASHgg version 1_1_0 w/MET syst for 25ns: 
+#python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia.json		-o DoubleEG 		-d lists_25ns_v1_1_0_wMETsys/Data
+#
+#python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o TTGJets  		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o TGJets 		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o WGToLNuG 		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_ewkbkg.json	-o ZGTo2LG   		-d lists_25ns_v1_1_0_wMETsys/MC
+#
+#python extractJSONS.py -i datasets/datasets_rereco74x_fromLivia_dy.json	-o DYJetsToLL		-d lists_25ns_v1_1_0_wMETsys/MC
+#
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o ttHJetToGG_M125	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o VBFHToGG_M-125	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o DiPhoton		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o QCD_Pt-30to40	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o QCD_Pt-40toInf	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o QCD_Pt-30toInf	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o GJet_Pt-20to40	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o GJet_Pt-40toInf	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o GluGluHToGG_M-125	-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o VHToGG_M125		-d lists_25ns_v1_1_0_wMETsys/MC
+#
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP600		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP800		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1000		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1200		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1400		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP1700		-d lists_25ns_v1_1_0_wMETsys/MC
+#python extractJSONS.py -i datasets/datasets_reminiaod74x_fromLivia.json	-o 2HDM_MZP2500		-d lists_25ns_v1_1_0_wMETsys/MC
 
 
 
