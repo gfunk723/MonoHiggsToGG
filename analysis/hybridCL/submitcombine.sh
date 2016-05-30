54007f5979c07dca5eb3d4467c69e1a1c878d669
Margaret Zientek <mez34@cornell.edu>
2016-05-30 13:59:52 +0200

    scripts for submiting and adding outputs easier

--- analysis/hybridCL/submitcombine.sh
+++ analysis/hybridCL/submitcombine.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
+mA0=300
 for mass in 600 800 1000 1200 1400 1700 2000 2500
   do
     echo $mass
-    python combine.py -i DataCard_2HDM_mZP${mass}.txt -d /afs/cern.ch/work/m/mzientek/public/HybridNewCL/2HDM_A0300 -q 1nh -M HybridNew --onews --begin ${mass} --end ${mass} -t 1000
+    python combine.py -i DataCard_2HDM_mZP${mass}_mA0${mA0}.txt -d /afs/cern.ch/work/m/mzientek/public/HybridNewCL/2HDM_A0${mA0}/MZP${mass} -q 1nh -M HybridNew --onews --begin ${mass} --end ${mass} -t 1000
   done
 
