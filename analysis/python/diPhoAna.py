8cca79c2ee8695b78f1f0a514b8d1358ca597883
Margaret Zientek <mez34@cornell.edu>
2016-08-03 14:29:05 +0200

    attempted to add pfcands (not stored in microAOD) to analyzer -- pfcands part commented out

--- analysis/python/diPhoAna.py
+++ analysis/python/diPhoAna.py
@@ -54,8 +54,26 @@ process.source = cms.Source("PoolSource",
                             fileNames=cms.untracked.vstring(
 	#"file:myMicroAODOutputFile.root"
 	# 76X
+
+	"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP600_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-600_MA0-300/160331_203427/0000/myMicroAODOutputFile_1.root", 
+	"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP600_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-600_MA0-300/160331_203427/0000/myMicroAODOutputFile_10.root",
+	"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP600_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-600_MA0-300/160331_203427/0000/myMicroAODOutputFile_2.root",
+
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP2000_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-2000_MA0-300/160331_204956/0000/myMicroAODOutputFile_1.root",
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP2000_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-2000_MA0-300/160331_204956/0000/myMicroAODOutputFile_10.root",
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP2000_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-2000_MA0-300/160331_204956/0000/myMicroAODOutputFile_2.root",
+
+
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP2500_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-2500_MA0-300/160331_205121/0000/myMicroAODOutputFile_1.root",
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP2500_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-2500_MA0-300/160331_205121/0000/myMicroAODOutputFile_10.root",
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP2500_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-2500_MA0-300/160331_205121/0000/myMicroAODOutputFile_2.root",
+
+
+
+
 	# vtx0:
-	"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP1000_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-1000_MA0-300/160331_204112/0000/myMicroAODOutputFile_1.root", 
+	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_PrivSig/1_3_0/MonoHgg_Zp2HDM_MZP1000_13TeV_76X/RunIIFall15DR76-1_3_0-25ns_MZp-1000_MA0-300/160331_204112/0000/myMicroAODOutputFile_1.root", 
+	#"/store/user/soffi/flashgg/RunIIFall15DR76-1_3_0-25ns_ext1/1_3_0/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8/RunIIFall15DR76-1_3_0-25ns_ext1-1_3_0-v0-RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/160330_084906/0000/myMicroAODOutputFile_1.root" 
 	#"/store/user/mzientek/flashgg/RunIIFall15DR76-1_3_0-25ns_Sig/1_3_0/ZprimeToA0hToA0chichihAA_2HDM_MZp-1200_MA0-300_13TeV-madgraph/RunIIFall15DR76-1_3_0-25ns_MZp-1200_MA0-300_v2/160401_091523/0000/myMicroAODOutputFile_2.root"
 	#"/store/user/soffi/flashgg/RunIIFall15DR76-1_3_0-25ns_ext1/1_3_0/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8/RunIIFall15DR76-1_3_0-25ns_ext1-1_3_0-v0-RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/160225_095156/0000/myMicroAODOutputFile_1.root",
 	#"/store/user/soffi/flashgg/RunIIFall15DR76-1_3_0-25ns_ext1/1_3_0/DiPhotonJets_MGG-80toInf_13TeV_amcatnloFXFX_pythia8/RunIIFall15DR76-1_3_0-25ns_ext1-1_3_0-v0-RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/160225_094833/0000/myMicroAODOutputFile_1.root", 
@@ -184,28 +202,29 @@ for i in range(0,maxJetCollections):
 
 
 process.diPhoAna = cms.EDAnalyzer('NewDiPhoAnalyzer',
-                                  VertexTag = cms.untracked.InputTag('offlineSlimmedPrimaryVertices'),
-				  METTag=cms.untracked.InputTag('slimmedMETs::FLASHggMicroAOD'),#
-                                  JetCorrectorTag = cms.InputTag("ak4PFCHSL1FastjetCorrector"),
-                                  inputTagJets= UnpackedJetCollectionVInputTag,            
-                                  ElectronTag=cms.InputTag('flashggSelectedElectrons'),    
-                                  MuonTag=cms.InputTag('flashggSelectedMuons'),            
-                                  bTag = cms.untracked.string(flashggBTag),                
-				  RhoTag = cms.InputTag('fixedGridRhoAll'),
-                                  genPhotonExtraTag = cms.InputTag("flashggGenPhotonsExtra"),    
-                                  DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons0vtx'),
-                                  #DiPhotonTag = cms.untracked.InputTag('flashggDiPhotons'),
-                                  DiPhotonBDTVtxTag = cms.untracked.InputTag('flashggDiPhotons'),
-                                  PileUpTag = cms.untracked.InputTag('slimmedAddPileupInfo'),
-                                  generatorInfo = cms.InputTag('generator'),
-                                  dopureweight = cms.untracked.int32(1),
-                                  bits         = cms.InputTag('TriggerResults::HLT'),
-                                  flags        = cms.InputTag(flag),
-				  sampleIndex  = cms.untracked.int32(100),
-                                  puWFileName  = cms.string('/afs/cern.ch/user/m/mzientek/public/pileupWeights_76X_vtx0.root'),  
-                                  xsec         = cms.untracked.double(1), #pb
-                                  kfac         = cms.untracked.double(1.),
-                                  sumDataset   = cms.untracked.double(1.0),   # chiara
+                                  VertexTag 		= cms.untracked.InputTag('offlineSlimmedPrimaryVertices'),
+				  METTag		= cms.untracked.InputTag('slimmedMETs::FLASHggMicroAOD'),#
+				  pfcands		= cms.InputTag("packedPFCandidates"),
+                                  JetCorrectorTag	= cms.InputTag("ak4PFCHSL1FastjetCorrector"),
+                                  inputTagJets		= UnpackedJetCollectionVInputTag,            
+                                  ElectronTag		= cms.InputTag('flashggSelectedElectrons'),    
+                                  MuonTag		= cms.InputTag('flashggSelectedMuons'),            
+                                  bTag 			= cms.untracked.string(flashggBTag),                
+				  RhoTag		= cms.InputTag('fixedGridRhoAll'),
+                                  genPhotonExtraTag 	= cms.InputTag("flashggGenPhotonsExtra"),    
+                                  DiPhotonTag 		= cms.untracked.InputTag('flashggDiPhotons0vtx'),
+                                  #DiPhotonTag 		= cms.untracked.InputTag('flashggDiPhotons'),
+                                  DiPhotonBDTVtxTag 	= cms.untracked.InputTag('flashggDiPhotons'),
+                                  PileUpTag 		= cms.untracked.InputTag('slimmedAddPileupInfo'),
+                                  generatorInfo 	= cms.InputTag('generator'),
+                                  dopureweight 		= cms.untracked.int32(1),
+                                  bits         		= cms.InputTag('TriggerResults::HLT'),
+                                  flags        		= cms.InputTag(flag),
+				  sampleIndex  		= cms.untracked.int32(100),
+                                  puWFileName  		= cms.string('/afs/cern.ch/user/m/mzientek/public/pileupWeights_76X_vtx0.root'),  
+                                  xsec         		= cms.untracked.double(1), #pb
+                                  kfac         		= cms.untracked.double(1.),
+                                  sumDataset   		= cms.untracked.double(1.0),   # chiara
                                   )
 
 #process.p = cms.Path( process.diPhoAna )     
