0ccb5ed166befe9b9652692734c36cf24b14dad0
Margaret Zientek <mez34@cornell.edu>
2016-08-07 16:47:21 +0200

    added 1d comparison plots of the different a0 limits

--- analysis/hybridCL/make2Dlimitplots.cc
+++ analysis/hybridCL/make2Dlimitplots.cc
@@ -31,16 +31,8 @@ void make2Dlimitplots(){
 void makePlots(TString inDir, TString outDir){
 
   // mZp masses 
-  std::vector<UInt_t> mass;
-  mass.push_back(600);
-  mass.push_back(800);
-  mass.push_back(1000);
-  mass.push_back(1200);
-  mass.push_back(1400);
-  mass.push_back(1700);
-  mass.push_back(2000);
-  mass.push_back(2500);
-  UInt_t nMasses = mass.size();
+  Double_t mass[8] = {600,800,1000,1200,1400,1700,2000,2500};
+  Int_t nMasses = 8;
 
   // pick up the higgsCombine files for each A0 mass
   std::vector<TFile* > higgsCombineFiles_MA0300;
@@ -58,16 +50,32 @@ void makePlots(TString inDir, TString outDir){
   higgsCombineFiles_MA0800.resize(nMasses);
 
   for (UInt_t n=0; n<nMasses; n++){
-    higgsCombineFiles_MA0300[n] = new TFile(Form("%sMA300/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),mass[n]));
-    higgsCombineFiles_MA0400[n] = new TFile(Form("%sMA400/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),mass[n]));
-    higgsCombineFiles_MA0500[n] = new TFile(Form("%sMA500/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),mass[n]));
-    higgsCombineFiles_MA0600[n] = new TFile(Form("%sMA600/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),mass[n]));
-    higgsCombineFiles_MA0700[n] = new TFile(Form("%sMA700/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),mass[n]));
-    higgsCombineFiles_MA0800[n] = new TFile(Form("%sMA800/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),mass[n]));
+    higgsCombineFiles_MA0300[n] = new TFile(Form("%sMA300/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),(Int_t)mass[n]));
+    higgsCombineFiles_MA0400[n] = new TFile(Form("%sMA400/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),(Int_t)mass[n]));
+    higgsCombineFiles_MA0500[n] = new TFile(Form("%sMA500/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),(Int_t)mass[n]));
+    higgsCombineFiles_MA0600[n] = new TFile(Form("%sMA600/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),(Int_t)mass[n]));
+    higgsCombineFiles_MA0700[n] = new TFile(Form("%sMA700/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),(Int_t)mass[n]));
+    higgsCombineFiles_MA0800[n] = new TFile(Form("%sMA800/higgsCombineTest.HybridNew.mH%i.root",inDir.Data(),(Int_t)mass[n]));
   }
 
  // pick up theory xsec
  TFile* theory_gz08 = new TFile("ScanPlot_gz08.root");
+
+ // setup 1D plots - expected
+ TGraph* limit300;
+ TGraph* limit400;
+ TGraph* limit500;
+ TGraph* limit600;
+ TGraph* limit700;
+ TGraph* limit800;
+
+ // setup 1D plots - observed
+ TGraph* limit300_obs;
+ TGraph* limit400_obs;
+ TGraph* limit500_obs;
+ TGraph* limit600_obs;
+ TGraph* limit700_obs;
+ TGraph* limit800_obs;
 
  // setup output plot
  TH2D * limits = new TH2D("limits","limits",8,0,8,6,0,6);
@@ -104,47 +112,42 @@ void makePlots(TString inDir, TString outDir){
  gStyle->SetOptStat(0);
  gStyle->SetPaintTextFormat("2.4f");
 
- std::vector< Double_t > limitval300;
- limitval300.resize(nMasses);
- std::vector< Double_t > limitval400;
- limitval400.resize(nMasses);
- std::vector< Double_t > limitval500;
- limitval500.resize(nMasses);
- std::vector< Double_t > limitval600;
- limitval600.resize(nMasses);
- std::vector< Double_t > limitval700;
- limitval700.resize(nMasses);
- std::vector< Double_t > limitval800;
- limitval800.resize(nMasses);
+ Double_t limitval300[nMasses];
+ Double_t limitval400[nMasses];
+ Double_t limitval500[nMasses];
+ Double_t limitval600[nMasses];
+ Double_t limitval700[nMasses];
+ Double_t limitval800[nMasses];
 
- std::vector< Double_t > limitval300_obs;
- limitval300_obs.resize(nMasses);
- std::vector< Double_t > limitval400_obs;
- limitval400_obs.resize(nMasses);
- std::vector< Double_t > limitval500_obs;
- limitval500_obs.resize(nMasses);
- std::vector< Double_t > limitval600_obs;
- limitval600_obs.resize(nMasses);
- std::vector< Double_t > limitval700_obs;
- limitval700_obs.resize(nMasses);
- std::vector< Double_t > limitval800_obs;
- limitval800_obs.resize(nMasses);
+ Double_t limitval300_obs[nMasses];
+ Double_t limitval400_obs[nMasses];
+ Double_t limitval500_obs[nMasses];
+ Double_t limitval600_obs[nMasses];
+ Double_t limitval700_obs[nMasses];
+ Double_t limitval800_obs[nMasses];
 
- std::vector< Double_t > xsecA0300;
- xsecA0300.resize(nMasses);
- std::vector< Double_t > xsecA0400;
- xsecA0400.resize(nMasses);
- std::vector< Double_t > xsecA0500;
- xsecA0500.resize(nMasses);
- std::vector< Double_t > xsecA0600;
- xsecA0600.resize(nMasses);
- std::vector< Double_t > xsecA0700;
- xsecA0700.resize(nMasses);
- std::vector< Double_t > xsecA0800;
- xsecA0800.resize(nMasses);
+ Double_t xsecA0300[nMasses];
+ Double_t xsecA0400[nMasses];
+ Double_t xsecA0500[nMasses];
+ Double_t xsecA0600[nMasses];
+ Double_t xsecA0700[nMasses];
+ Double_t xsecA0800[nMasses];
 
+ Double_t explimit300[nMasses];
+ Double_t explimit400[nMasses];
+ Double_t explimit500[nMasses];
+ Double_t explimit600[nMasses];
+ Double_t explimit700[nMasses];
+ Double_t explimit800[nMasses];
+ 
+ Double_t obslimit300[nMasses];
+ Double_t obslimit400[nMasses];
+ Double_t obslimit500[nMasses];
+ Double_t obslimit600[nMasses];
+ Double_t obslimit700[nMasses];
+ Double_t obslimit800[nMasses];
 
- for (UInt_t n=0; n<nMasses; n++){
+ for (Int_t n=0; n<nMasses; n++){
    getLimits(higgsCombineFiles_MA0300[n],limitval300[n],0.5); 
    getLimits(higgsCombineFiles_MA0400[n],limitval400[n],0.5); 
    getLimits(higgsCombineFiles_MA0500[n],limitval500[n],0.5); 
@@ -160,12 +163,26 @@ void makePlots(TString inDir, TString outDir){
    getLimits(higgsCombineFiles_MA0700[n],limitval700_obs[n],-1); 
    getLimits(higgsCombineFiles_MA0800[n],limitval800_obs[n],-1); 
 
-   getXsec(theory_gz08,300,mass[n],xsecA0300[n]);
-   getXsec(theory_gz08,400,mass[n],xsecA0400[n]);
-   getXsec(theory_gz08,500,mass[n],xsecA0500[n]);
-   getXsec(theory_gz08,600,mass[n],xsecA0600[n]);
-   getXsec(theory_gz08,700,mass[n],xsecA0700[n]);
-   getXsec(theory_gz08,800,mass[n],xsecA0800[n]);
+   getXsec(theory_gz08,300,(Int_t)mass[n],xsecA0300[n]);
+   getXsec(theory_gz08,400,(Int_t)mass[n],xsecA0400[n]);
+   getXsec(theory_gz08,500,(Int_t)mass[n],xsecA0500[n]);
+   getXsec(theory_gz08,600,(Int_t)mass[n],xsecA0600[n]);
+   getXsec(theory_gz08,700,(Int_t)mass[n],xsecA0700[n]);
+   getXsec(theory_gz08,800,(Int_t)mass[n],xsecA0800[n]);
+
+   explimit300[n] = limitval300[n]*2*xsecA0300[n];
+   explimit400[n] = limitval400[n]*2*xsecA0400[n];
+   explimit500[n] = limitval500[n]*2*xsecA0500[n];
+   explimit600[n] = limitval600[n]*2*xsecA0600[n];
+   explimit700[n] = limitval700[n]*2*xsecA0700[n];
+   explimit800[n] = limitval800[n]*2*xsecA0800[n];
+
+   obslimit300[n] = limitval300_obs[n]*2*xsecA0300[n];
+   obslimit400[n] = limitval400_obs[n]*2*xsecA0400[n];
+   obslimit500[n] = limitval500_obs[n]*2*xsecA0500[n];
+   obslimit600[n] = limitval600_obs[n]*2*xsecA0600[n];
+   obslimit700[n] = limitval700_obs[n]*2*xsecA0700[n];
+   obslimit800[n] = limitval800_obs[n]*2*xsecA0800[n];
 
    // fill limit plot
    limits->Fill(((Double_t)n+0.5),0.5,limitval300[n]*2*xsecA0300[n]);
@@ -174,35 +191,157 @@ void makePlots(TString inDir, TString outDir){
    limits->Fill(((Double_t)n+0.5),3.5,limitval600[n]*2*xsecA0600[n]);
    limits->Fill(((Double_t)n+0.5),4.5,limitval700[n]*2*xsecA0700[n]);
    limits->Fill(((Double_t)n+0.5),5.5,limitval800[n]*2*xsecA0800[n]);
-   
+
    obslimits->Fill(((Double_t)n+0.5),0.5,limitval300_obs[n]*2*xsecA0300[n]);
    obslimits->Fill(((Double_t)n+0.5),1.5,limitval400_obs[n]*2*xsecA0400[n]);
    obslimits->Fill(((Double_t)n+0.5),2.5,limitval500_obs[n]*2*xsecA0500[n]);
    obslimits->Fill(((Double_t)n+0.5),3.5,limitval600_obs[n]*2*xsecA0600[n]);
    obslimits->Fill(((Double_t)n+0.5),4.5,limitval700_obs[n]*2*xsecA0700[n]);
    obslimits->Fill(((Double_t)n+0.5),5.5,limitval800_obs[n]*2*xsecA0800[n]);
-   
  }
 
- // draw plot
- limits->Draw("COLZ TEXT"); 
+ limit300 = new TGraph(nMasses,mass,explimit300);
+ limit400 = new TGraph(nMasses,mass,explimit400);
+ limit500 = new TGraph(nMasses,mass,explimit500);
+ limit600 = new TGraph(nMasses,mass,explimit600);
+ limit700 = new TGraph(nMasses,mass,explimit700);
+ limit800 = new TGraph(nMasses,mass,explimit800);
 
+ //styling
+ limit300->GetXaxis()->SetTitle("m_{Z'} [GeV]");
+ limit300->GetYaxis()->SetTitle("UL [fb]");
+ limit300->SetTitle("");
+ limit300->SetMaximum(2);
+ limit300->SetMinimum(0.001);
+ limit300->SetLineWidth(2);
+ limit400->SetLineWidth(2);
+ limit500->SetLineWidth(2);
+ limit600->SetLineWidth(2);
+ limit700->SetLineWidth(2);
+ limit800->SetLineWidth(2);
+ limit300->SetMarkerStyle(8);
+ limit400->SetMarkerStyle(8);
+ limit500->SetMarkerStyle(8);
+ limit600->SetMarkerStyle(8);
+ limit700->SetMarkerStyle(8);
+ limit800->SetMarkerStyle(8);
+ // set up colors to match Hbb
+ limit300->SetLineColor(kBlack);
+ limit400->SetLineColor(kCyan);
+ limit500->SetLineColor(kGreen);
+ limit600->SetLineColor(kBlue);
+ limit700->SetLineColor(kYellow);
+ limit800->SetLineColor(kMagenta);
+ limit300->SetMarkerColor(kBlack);
+ limit400->SetMarkerColor(kCyan);
+ limit500->SetMarkerColor(kGreen);
+ limit600->SetMarkerColor(kBlue);
+ limit700->SetMarkerColor(kYellow);
+ limit800->SetMarkerColor(kMagenta);
+
+ 
+ limit300_obs = new TGraph(nMasses,mass,obslimit300);
+ limit400_obs = new TGraph(nMasses,mass,obslimit400);
+ limit500_obs = new TGraph(nMasses,mass,obslimit500);
+ limit600_obs = new TGraph(nMasses,mass,obslimit600);
+ limit700_obs = new TGraph(nMasses,mass,obslimit700);
+ limit800_obs = new TGraph(nMasses,mass,obslimit800);
+
+ //styling
+ limit300_obs->GetXaxis()->SetTitle("m_{Z'} [GeV]");
+ limit300_obs->GetYaxis()->SetTitle("UL [fb]");
+ limit300_obs->SetTitle("");
+ limit300_obs->SetMaximum(2);
+ limit300_obs->SetMinimum(0.001);
+ limit300_obs->SetLineWidth(2);
+ limit400_obs->SetLineWidth(2);
+ limit500_obs->SetLineWidth(2);
+ limit600_obs->SetLineWidth(2);
+ limit700_obs->SetLineWidth(2);
+ limit800_obs->SetLineWidth(2);
+ limit300_obs->SetMarkerStyle(8);
+ limit400_obs->SetMarkerStyle(8);
+ limit500_obs->SetMarkerStyle(8);
+ limit600_obs->SetMarkerStyle(8);
+ limit700_obs->SetMarkerStyle(8);
+ limit800_obs->SetMarkerStyle(8);
+ // set up colors to match Hbb
+ limit300_obs->SetMarkerColor(kBlack);
+ limit400_obs->SetMarkerColor(kCyan);
+ limit500_obs->SetMarkerColor(kGreen);
+ limit600_obs->SetMarkerColor(kBlue);
+ limit700_obs->SetMarkerColor(kYellow);
+ limit800_obs->SetMarkerColor(kMagenta);
+ limit300_obs->SetLineColor(kBlack);
+ limit400_obs->SetLineColor(kCyan);
+ limit500_obs->SetLineColor(kGreen);
+ limit600_obs->SetLineColor(kBlue);
+ limit700_obs->SetLineColor(kYellow);
+ limit800_obs->SetLineColor(kMagenta);
+ 
+ // draw expected limits plot
+ limits->Draw("COLZ TEXT"); 
  // save plot
  CMS_lumi(c,false,7);
  c->cd();
  c->SaveAs(Form("%s/limits2D_2HDM_exp.png",outDir.Data()));
  c->SaveAs(Form("%s/limits2D_2HDM_exp.pdf",outDir.Data()));
 
+ // draw observed limits plot
  obslimits->Draw("COLZ TEXT"); 
-
  // save plot
  CMS_lumi(c,false,7);
  c->cd();
  c->SaveAs(Form("%s/limits2D_2HDM_obs.png",outDir.Data()));
  c->SaveAs(Form("%s/limits2D_2HDM_obs.pdf",outDir.Data()));
 
+ TLegend* leg = new TLegend(0.7,0.65,0.9,0.85); // (x1,y1,x2,y2)
+ leg->SetBorderSize(4);
+ leg->SetLineWidth(2);
+ //leg->SetFillColor(0);
+ //leg->SetFillStyle(0);
+ leg->AddEntry(limit300,"m_{A0}=300GeV","pl");
+ leg->AddEntry(limit400,"m_{A0}=400GeV","pl");
+ leg->AddEntry(limit500,"m_{A0}=500GeV","pl");
+ leg->AddEntry(limit600,"m_{A0}=600GeV","pl");
+ leg->AddEntry(limit700,"m_{A0}=700GeV","pl");
+ leg->AddEntry(limit800,"m_{A0}=800GeV","pl");
+ leg->SetTextSize(0.03);
+
+ // draw 1D comparisons --expected
+ c->Clear();
+ c->SetLogy(1);
+ limit300->Draw("APL");
+ limit400->Draw("PL SAME");
+ limit500->Draw("PL SAME");
+ limit600->Draw("PL SAME");
+ limit700->Draw("PL SAME");
+ limit800->Draw("PL SAME");
+ leg->Draw("SAME");
+ CMS_lumi(c,false,7);
+ c->cd();
+ c->SaveAs(Form("%s/limits_comparison_2HDM_exp.png",outDir.Data()));
+ c->SaveAs(Form("%s/limits_comparison_2HDM_exp.pdf",outDir.Data()));
+
+ // draw 1D comparisons --observed
+ c->Clear();
+ c->SetLogy(1);
+ limit300_obs->Draw("APL");
+ limit400_obs->Draw("PL SAME");
+ limit500_obs->Draw("PL SAME");
+ limit600_obs->Draw("PL SAME");
+ limit700_obs->Draw("PL SAME");
+ limit800_obs->Draw("PL SAME");
+ leg->Draw("SAME");
+ CMS_lumi(c,false,7);
+ c->cd();
+ c->SaveAs(Form("%s/limits_comparison_2HDM_obs.png",outDir.Data()));
+ c->SaveAs(Form("%s/limits_comparison_2HDM_obs.pdf",outDir.Data()));
+
  delete c;
 
+
+ // make plot with both expected and observed on same graph
  TCanvas* cboth = new TCanvas("cboth","");
  cboth->cd();
  gStyle->SetOptStat(0);
@@ -253,6 +392,7 @@ void makePlots(TString inDir, TString outDir){
  c->SaveAs(Form("%s/limits2D_2HDM_ExpAndObs.png",outDir.Data()));
  c->SaveAs(Form("%s/limits2D_2HDM_ExpAndObs.pdf",outDir.Data()));
  delete cboth;
+
 
 }
 
