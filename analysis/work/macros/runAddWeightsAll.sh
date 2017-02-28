1eb6256bfb0a3537dd2e412a6e73122a46c9134c
Margaret Zientek <mez34@cornell.edu>
2017-02-28 14:43:41 +0100

    added missing samples to macros

--- analysis/work/macros/runAddWeightsAll.sh
+++ analysis/work/macros/runAddWeightsAll.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
-path="/afs/cern.ch/work/m/mzientek/public/25ns_v80X_v5/"
+path="/afs/cern.ch/work/m/mzientek/public/25ns_v80X_moriond17_v1/"
 #path="../../macro/data/25ns_v80X_v5/"
 weight=$1
+
 
 root -l -b << EOF
 .L ../scripts/addWeights.C++
@@ -18,7 +19,7 @@ addWeights(${weight},"${path}","Run2016H");
 addWeights(${weight},"${path}","GluGluHToGG");
 addWeights(${weight},"${path}","VHToGG");
 addWeights(${weight},"${path}","VBFHToGG");
-addWeights(${weight},"${path}","ttHJetToGG");
+addWeights(${weight},"${path}","ttHToGG");
 
 addWeights(${weight},"${path}","DiPhoton");
 addWeights(${weight},"${path}","QCD_Pt-30toInf");
@@ -35,6 +36,41 @@ addWeights(${weight},"${path}","WJetsToLNu");
 addWeights(${weight},"${path}","WZTo2L2Q");
 addWeights(${weight},"${path}","ZGTo2LG");
 addWeights(${weight},"${path}","ZZTo2L2Q");
+
+addWeights(${weight},"${path}","ScalarZp_mZP10000_mChi1000");
+addWeights(${weight},"${path}","ScalarZp_mZP10000_mChi10");
+addWeights(${weight},"${path}","ScalarZp_mZP10000_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP10000_mChi150");
+addWeights(${weight},"${path}","ScalarZp_mZP10000_mChi500");
+addWeights(${weight},"${path}","ScalarZp_mZP10000_mChi50");
+addWeights(${weight},"${path}","ScalarZp_mZP1000_mChi1000");
+addWeights(${weight},"${path}","ScalarZp_mZP1000_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP1000_mChi150");
+addWeights(${weight},"${path}","ScalarZp_mZP100_mChi10");
+addWeights(${weight},"${path}","ScalarZp_mZP100_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP10_mChi1000");
+addWeights(${weight},"${path}","ScalarZp_mZP10_mChi10");
+addWeights(${weight},"${path}","ScalarZp_mZP10_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP10_mChi150");
+addWeights(${weight},"${path}","ScalarZp_mZP10_mChi500");
+addWeights(${weight},"${path}","ScalarZp_mZP10_mChi50");
+addWeights(${weight},"${path}","ScalarZp_mZP15_mChi10");
+addWeights(${weight},"${path}","ScalarZp_mZP200_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP200_mChi150");
+addWeights(${weight},"${path}","ScalarZp_mZP200_mChi50");
+addWeights(${weight},"${path}","ScalarZp_mZP20_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP295_mChi150");
+addWeights(${weight},"${path}","ScalarZp_mZP300_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP300_mChi50");
+addWeights(${weight},"${path}","ScalarZp_mZP500_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP500_mChi150");
+addWeights(${weight},"${path}","ScalarZp_mZP500_mChi500");
+addWeights(${weight},"${path}","ScalarZp_mZP50_mChi10");
+addWeights(${weight},"${path}","ScalarZp_mZP50_mChi1");
+addWeights(${weight},"${path}","ScalarZp_mZP50_mChi50");
+addWeights(${weight},"${path}","ScalarZp_mZP95_mChi50");
+addWeights(${weight},"${path}","ScalarZp_mZP995_mChi500");
+
 addWeights(${weight},"${path}","BaryonicZp_mZP10000_mChi1000");
 addWeights(${weight},"${path}","BaryonicZp_mZP10000_mChi10");
 addWeights(${weight},"${path}","BaryonicZp_mZP10000_mChi150");
@@ -69,26 +105,36 @@ addWeights(${weight},"${path}","BaryonicZp_mZP995_mChi500");
 
 addWeights(${weight},"${path}","2HDM_mZP1000_mA0300");
 addWeights(${weight},"${path}","2HDM_mZP1000_mA0400");
+addWeights(${weight},"${path}","2HDM_mZP1000_mA0500");
+addWeights(${weight},"${path}","2HDM_mZP1000_mA0600");
 addWeights(${weight},"${path}","2HDM_mZP1000_mA0700");
+addWeights(${weight},"${path}","2HDM_mZP1000_mA0800");
 addWeights(${weight},"${path}","2HDM_mZP1200_mA0300");
 addWeights(${weight},"${path}","2HDM_mZP1200_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP1200_mA0500");
 addWeights(${weight},"${path}","2HDM_mZP1200_mA0600");
+addWeights(${weight},"${path}","2HDM_mZP1200_mA0700");
 addWeights(${weight},"${path}","2HDM_mZP1200_mA0800");
 addWeights(${weight},"${path}","2HDM_mZP1400_mA0300");
 addWeights(${weight},"${path}","2HDM_mZP1400_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP1400_mA0500");
 addWeights(${weight},"${path}","2HDM_mZP1400_mA0600");
 addWeights(${weight},"${path}","2HDM_mZP1400_mA0700");
+addWeights(${weight},"${path}","2HDM_mZP1400_mA0800");
 addWeights(${weight},"${path}","2HDM_mZP1700_mA0300");
 addWeights(${weight},"${path}","2HDM_mZP1700_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP1700_mA0500");
+addWeights(${weight},"${path}","2HDM_mZP1700_mA0600");
 addWeights(${weight},"${path}","2HDM_mZP1700_mA0700");
 addWeights(${weight},"${path}","2HDM_mZP1700_mA0800");
 addWeights(${weight},"${path}","2HDM_mZP2000_mA0300");
 addWeights(${weight},"${path}","2HDM_mZP2000_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP2000_mA0500");
+addWeights(${weight},"${path}","2HDM_mZP2000_mA0600");
+addWeights(${weight},"${path}","2HDM_mZP2000_mA0700");
+addWeights(${weight},"${path}","2HDM_mZP2000_mA0800");
 addWeights(${weight},"${path}","2HDM_mZP2500_mA0300");
+addWeights(${weight},"${path}","2HDM_mZP2500_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP2500_mA0500");
 addWeights(${weight},"${path}","2HDM_mZP2500_mA0600");
 addWeights(${weight},"${path}","2HDM_mZP2500_mA0700");
@@ -96,7 +142,9 @@ addWeights(${weight},"${path}","2HDM_mZP2500_mA0800");
 addWeights(${weight},"${path}","2HDM_mZP600_mA0300");
 addWeights(${weight},"${path}","2HDM_mZP600_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP800_mA0300");
+addWeights(${weight},"${path}","2HDM_mZP800_mA0400");
 addWeights(${weight},"${path}","2HDM_mZP800_mA0500");
+addWeights(${weight},"${path}","2HDM_mZP800_mA0600");
 
 .q
 
--- analysis/work/macros/runSkimAll.sh
+++ analysis/work/macros/runSkimAll.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-path="/afs/cern.ch/work/m/mzientek/public/25ns_v80X_v5/"
+path="/afs/cern.ch/work/m/mzientek/public/25ns_v80X_moriond17_v1/"
 #path="../macro/data/25ns_v80X_v5/"
 
 root -l -b << EOF
@@ -60,30 +60,68 @@ skim("${path}","BaryonicZp_mZP50_mChi10");
 skim("${path}","BaryonicZp_mZP50_mChi1");
 skim("${path}","BaryonicZp_mZP995_mChi500");
 
-
+skim("${path}","ScalarZp_mZP10000_mChi1");
+skim("${path}","ScalarZp_mZP10000_mChi10");
+skim("${path}","ScalarZp_mZP10000_mChi1000");
+skim("${path}","ScalarZp_mZP10000_mChi150");
+skim("${path}","ScalarZp_mZP10000_mChi50");
+skim("${path}","ScalarZp_mZP10000_mChi500");
+skim("${path}","ScalarZp_mZP1000_mChi1");
+skim("${path}","ScalarZp_mZP1000_mChi150");
+skim("${path}","ScalarZp_mZP100_mChi1");
+skim("${path}","ScalarZp_mZP100_mChi10");
+skim("${path}","ScalarZp_mZP10_mChi1");
+skim("${path}","ScalarZp_mZP10_mChi10");
+skim("${path}","ScalarZp_mZP10_mChi1000");
+skim("${path}","ScalarZp_mZP10_mChi500");
+skim("${path}","ScalarZp_mZP200_mChi1");
+skim("${path}","ScalarZp_mZP200_mChi150");
+skim("${path}","ScalarZp_mZP200_mChi50");
+skim("${path}","ScalarZp_mZP20_mChi1");
+skim("${path}","ScalarZp_mZP295_mChi150");
+skim("${path}","ScalarZp_mZP300_mChi1");
+skim("${path}","ScalarZp_mZP300_mChi50");
+skim("${path}","ScalarZp_mZP500_mChi1");
+skim("${path}","ScalarZp_mZP500_mChi150");
+skim("${path}","ScalarZp_mZP500_mChi500");
+skim("${path}","ScalarZp_mZP50_mChi1");
+skim("${path}","ScalarZp_mZP50_mChi10");
+skim("${path}","ScalarZp_mZP50_mChi50");
+skim("${path}","ScalarZp_mZP95_mChi50");
+skim("${path}","ScalarZp_mZP995_mChi500");
 
 skim("${path}","2HDM_mZP1000_mA0300");
 skim("${path}","2HDM_mZP1000_mA0400");
+skim("${path}","2HDM_mZP1000_mA0500");
+skim("${path}","2HDM_mZP1000_mA0600");
 skim("${path}","2HDM_mZP1000_mA0700");
+skim("${path}","2HDM_mZP1000_mA0800");
 skim("${path}","2HDM_mZP1200_mA0300");
 skim("${path}","2HDM_mZP1200_mA0400");
 skim("${path}","2HDM_mZP1200_mA0500");
 skim("${path}","2HDM_mZP1200_mA0600");
+skim("${path}","2HDM_mZP1200_mA0700");
 skim("${path}","2HDM_mZP1200_mA0800");
 skim("${path}","2HDM_mZP1400_mA0300");
 skim("${path}","2HDM_mZP1400_mA0400");
 skim("${path}","2HDM_mZP1400_mA0500");
 skim("${path}","2HDM_mZP1400_mA0600");
 skim("${path}","2HDM_mZP1400_mA0700");
+skim("${path}","2HDM_mZP1400_mA0800");
 skim("${path}","2HDM_mZP1700_mA0300");
 skim("${path}","2HDM_mZP1700_mA0400");
 skim("${path}","2HDM_mZP1700_mA0500");
+skim("${path}","2HDM_mZP1700_mA0600");
 skim("${path}","2HDM_mZP1700_mA0700");
 skim("${path}","2HDM_mZP1700_mA0800");
 skim("${path}","2HDM_mZP2000_mA0300");
 skim("${path}","2HDM_mZP2000_mA0400");
 skim("${path}","2HDM_mZP2000_mA0500");
+skim("${path}","2HDM_mZP2000_mA0600");
+skim("${path}","2HDM_mZP2000_mA0700");
+skim("${path}","2HDM_mZP2000_mA0800");
 skim("${path}","2HDM_mZP2500_mA0300");
+skim("${path}","2HDM_mZP2500_mA0400");
 skim("${path}","2HDM_mZP2500_mA0500");
 skim("${path}","2HDM_mZP2500_mA0600");
 skim("${path}","2HDM_mZP2500_mA0700");
@@ -91,8 +129,9 @@ skim("${path}","2HDM_mZP2500_mA0800");
 skim("${path}","2HDM_mZP600_mA0300");
 skim("${path}","2HDM_mZP600_mA0400");
 skim("${path}","2HDM_mZP800_mA0300");
+skim("${path}","2HDM_mZP800_mA0400");
 skim("${path}","2HDM_mZP800_mA0500");
-
+skim("${path}","2HDM_mZP800_mA0600");
 .q
 
 EOF
