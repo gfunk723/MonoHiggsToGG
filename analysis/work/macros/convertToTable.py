a0dc7e5d9050bd6bbea8db7ff29d18d84b6a682b
Margaret Zientek <mez34@cornell.edu>
2017-02-28 14:33:05 +0100

    add scalar eff to output table

--- analysis/work/macros/convertToTable.py
+++ analysis/work/macros/convertToTable.py
@@ -28,7 +28,19 @@ def AllGlobal(fname):
   effbary15 = ["-"]*6
   effbary10 = ["-"]*6
 
-
+  effscal10000 = ["-"]*6
+  effscal1000 = ["-"]*6
+  effscal995 = ["-"]*6
+  effscal500 = ["-"]*6
+  effscal300 = ["-"]*6
+  effscal295 = ["-"]*6
+  effscal200 = ["-"]*6
+  effscal100 = ["-"]*6
+  effscal95 = ["-"]*6  
+  effscal50 = ["-"]*6
+  effscal20 = ["-"]*6
+  effscal15 = ["-"]*6
+  effscal10 = ["-"]*6
 
   for line in fin:
     line = line.strip()
@@ -313,9 +325,182 @@ def AllGlobal(fname):
            elif "mChi1" in string[0]:
              effbary10[0]=string[1]
             
-            
+    elif "Scalar" in string[0]:
+       if "mZP10000" in string[0]: #mZP10000
+           if "mChi1000" in string[0]:
+             effscal10000[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal10000[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal10000[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal10000[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal10000[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal10000[0]=string[1]
+       elif "mZP1000" in string[0]: #mZP1000
+           if "mChi1000" in string[0]:
+             effscal1000[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal1000[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal1000[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal1000[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal1000[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal1000[0]=string[1]
+       elif "mZP995" in string[0]: #mZP995
+           if "mChi1000" in string[0]:
+             effscal995[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal995[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal995[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal995[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal995[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal995[0]=string[1]
+       elif "mZP500" in string[0]: #mZP500
+           if "mChi1000" in string[0]:
+             effscal500[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal500[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal500[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal500[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal500[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal500[0]=string[1]
+       elif "mZP300" in string[0]: #mZP300
+           if "mChi1000" in string[0]:
+             effscal300[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal300[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal300[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal300[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal300[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal300[0]=string[1]
+       elif "mZP295" in string[0]: #mZP295
+           if "mChi1000" in string[0]:
+             effscal295[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal295[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal295[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal295[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal295[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal295[0]=string[1]
+       elif "mZP200" in string[0]: #mZP200
+           if "mChi1000" in string[0]:
+             effscal200[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal200[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal200[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal200[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal200[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal200[0]=string[1]
+       elif "mZP100" in string[0]: #mZP100
+           if "mChi1000" in string[0]:
+             effscal100[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal100[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal100[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal100[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal100[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal100[0]=string[1]
+       elif "mZP95" in string[0]: #mZP95
+           if "mChi1000" in string[0]:
+             effscal95[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal95[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal95[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal95[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal95[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal95[0]=string[1]
+       elif "mZP50" in string[0]: #mZP50
+           if "mChi1000" in string[0]:
+             effscal50[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal50[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal50[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal50[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal50[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal50[0]=string[1]
+       elif "mZP20" in string[0]: #mZP20
+           if "mChi1000" in string[0]:
+             effscal20[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal20[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal20[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal20[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal20[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal20[0]=string[1]
+       elif "mZP15" in string[0]: #mZP15
+           if "mChi1000" in string[0]:
+             effscal15[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal15[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal15[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal15[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal15[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal100[0]=string[1]
+       elif "mZP10" in string[0]: #mZP10
+           if "mChi1000" in string[0]:
+             effscal10[5]=string[1]
+           elif "mChi500" in string[0]:
+             effscal10[4]=string[1]
+           elif "mChi150" in string[0]:
+             effscal10[3]=string[1]
+           elif "mChi50" in string[0]:
+             effscal10[2]=string[1]
+           elif "mChi10" in string[0]:
+             effscal10[1]=string[1]
+           elif "mChi1" in string[0]:
+             effscal10[0]=string[1]
 
 
+  # WRITE OUT TO A NEW TABLE
+
+  fout.write("\\hline \n")
+  fout.write("% 2HDM Efficiencies \n")
   fout.write("$m_{A}$ [GeV] & 300 & 400 & 500 & 600 & 700 & 800 \\\\   \n")
   fout.write("$m_{Z'}$ [GeV] &    &     &     &     &     &     \\\\   \n")
   fout.write("600 & "+eff2hdm600[0]+" & "+eff2hdm600[1]+" & "+eff2hdm600[2]+" & "+eff2hdm600[3]+" & "+eff2hdm600[4]+" & "+eff2hdm600[5]+" \\\\ \n")
@@ -326,8 +511,10 @@ def AllGlobal(fname):
   fout.write("1700 & "+eff2hdm1700[0]+" & "+eff2hdm1700[1]+" & "+eff2hdm1700[2]+" & "+eff2hdm1700[3]+" & "+eff2hdm1700[4]+" & "+eff2hdm1700[5]+" \\\\ \n")
   fout.write("2000 & "+eff2hdm2000[0]+" & "+eff2hdm2000[1]+" & "+eff2hdm2000[2]+" & "+eff2hdm2000[3]+" & "+eff2hdm2000[4]+" & "+eff2hdm2000[5]+" \\\\ \n")
   fout.write("2500 & "+eff2hdm2500[0]+" & "+eff2hdm2500[1]+" & "+eff2hdm2500[2]+" & "+eff2hdm2500[3]+" & "+eff2hdm2500[4]+" & "+eff2hdm2500[5]+" \\\\ \n")
-  fout.write("\n")
-  fout.write("$m_{\\Chi}$ [GeV] & 1 & 10 & 50 & 150 & 500 & 1000 \\\\   \n")
+  fout.write("\\hline \n")
+  fout.write("\\hline \n")
+  fout.write("% Baryonic Model Efficiencies \n")
+  fout.write("$m_{\\chi}$ [GeV] & 1 & 10 & 50 & 150 & 500 & 1000 \\\\   \n")
   fout.write("$m_{Z'}$ [GeV]    &   &    &    &     &     &      \\\\   \n")
   fout.write("10 & "+effbary10[0]+" & "+effbary10[1]+" & "+effbary10[2]+" & "+effbary10[3]+" & "+effbary10[4]+" & "+effbary10[5]+" \\\\ \n")
   fout.write("15 & "+effbary15[0]+" & "+effbary15[1]+" & "+effbary15[2]+" & "+effbary15[3]+" & "+effbary15[4]+" & "+effbary15[5]+" \\\\ \n")
@@ -343,7 +530,24 @@ def AllGlobal(fname):
   fout.write("1995 & "+effbary1995[0]+" & "+effbary1995[1]+" & "+effbary1995[2]+" & "+effbary1995[3]+" & "+effbary1995[4]+" & "+effbary1995[5]+" \\\\ \n")
   fout.write("2000 & "+effbary2000[0]+" & "+effbary2000[1]+" & "+effbary2000[2]+" & "+effbary2000[3]+" & "+effbary2000[4]+" & "+effbary2000[5]+" \\\\ \n")
   fout.write("10000 & "+effbary10000[0]+" & "+effbary10000[1]+" & "+effbary10000[2]+" & "+effbary10000[3]+" & "+effbary10000[4]+" & "+effbary10000[5]+" \\\\ \n")
-
+  fout.write("\\hline \n")
+  fout.write("\\hline \n")
+  fout.write("% Scalar Model Efficiencies \n")
+  fout.write("$m_{\\chi}$ [GeV] & 1 & 10 & 50 & 150 & 500 & 1000 \\\\   \n")
+  fout.write("$m_{Z'}$ [GeV]    &   &    &    &     &     &      \\\\   \n")
+  fout.write("10 & "+effscal10[0]+" & "+effscal10[1]+" & "+effscal10[2]+" & "+effscal10[3]+" & "+effscal10[4]+" & "+effscal10[5]+" \\\\ \n")
+  fout.write("20 & "+effscal20[0]+" & "+effscal20[1]+" & "+effscal20[2]+" & "+effscal20[3]+" & "+effscal20[4]+" & "+effscal20[5]+" \\\\ \n")
+  fout.write("50 & "+effscal50[0]+" & "+effscal50[1]+" & "+effscal50[2]+" & "+effscal50[3]+" & "+effscal50[4]+" & "+effscal50[5]+" \\\\ \n")
+  fout.write("95 & "+effscal95[0]+" & "+effscal95[1]+" & "+effscal95[2]+" & "+effscal95[3]+" & "+effscal95[4]+" & "+effscal95[5]+" \\\\ \n")
+  fout.write("100 & "+effscal100[0]+" & "+effscal100[1]+" & "+effscal100[2]+" & "+effscal100[3]+" & "+effscal100[4]+" & "+effscal100[5]+" \\\\ \n")
+  fout.write("200 & "+effscal200[0]+" & "+effscal200[1]+" & "+effscal200[2]+" & "+effscal200[3]+" & "+effscal200[4]+" & "+effscal200[5]+" \\\\ \n")
+  fout.write("295 & "+effscal295[0]+" & "+effscal295[1]+" & "+effscal295[2]+" & "+effscal295[3]+" & "+effscal295[4]+" & "+effscal295[5]+" \\\\ \n")
+  fout.write("300 & "+effscal300[0]+" & "+effscal300[1]+" & "+effscal300[2]+" & "+effscal300[3]+" & "+effscal300[4]+" & "+effscal300[5]+" \\\\ \n")
+  fout.write("500 & "+effscal500[0]+" & "+effscal500[1]+" & "+effscal500[2]+" & "+effscal500[3]+" & "+effscal500[4]+" & "+effscal500[5]+" \\\\ \n")
+  fout.write("995 & "+effscal995[0]+" & "+effscal995[1]+" & "+effscal995[2]+" & "+effscal995[3]+" & "+effscal995[4]+" & "+effscal995[5]+" \\\\ \n")
+  fout.write("1000 & "+effscal1000[0]+" & "+effscal1000[1]+" & "+effscal1000[2]+" & "+effscal1000[3]+" & "+effscal1000[4]+" & "+effscal1000[5]+" \\\\ \n")
+  fout.write("10000 & "+effscal10000[0]+" & "+effscal10000[1]+" & "+effscal10000[2]+" & "+effscal10000[3]+" & "+effscal10000[4]+" & "+effscal10000[5]+" \\\\ \n")
+  fout.write("\\hline \n")
 
 if __name__ == "__main__":
   AllGlobal("PlainEff.tex")
