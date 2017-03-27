777a97020119e7088f0739b34beca62c402fa548
Margaret Zientek <mez34@cornell.edu>
2017-03-27 18:52:38 +0200

    added scripts to resubmit jobs that don't make log files

--- analysis/scripts/resubmit.sh
+++ analysis/scripts/resubmit.sh
@@ -3,6 +3,7 @@
 # ---------------------------------------------------------
 # Run by caling:
 # ./resubmit.sh job_XXXXXXX_XX SampleName
+# check size of output files: find DIRECTORY -type f -size -10k
 # ---------------------------------------------------------
 
 DIR=$1
@@ -18,15 +19,17 @@ logg="log"
 src="src"
 for i in $FILES
 do
-    if grep -ir FallbackFileOpenError >/dev/null $i
+    #if grep -ir FallbackFileOpenError >/dev/null $i
+    if grep -ir "exit code" >/dev/null $i
+    #if grep -ir "Begin Fatal" >/dev/null $i
 	then
 	logfile=$i
 	srcfile=$(echo $i | sed "s/$log/$src/g")
 	srcfile=$(echo $srcfile | sed "s/"$NAME"_/submit_/g")
 	srcfile=$(echo $srcfile | sed "s/$logg/$src/g")
 	logfile=$(echo $logfile | sed "s/logOld/log/g")
-	echo bsub -q 8nh -o $logfile source /afs/cern.ch/user/m/mzientek/private/MonoHggCMSSW80X/CMSSW_8_0_8_patch1/src/MonoHiggsToGG/analysis/scripts/$srcfile 
-	bsub -q 8nh -o $logfile source /afs/cern.ch/user/m/mzientek/private/MonoHggCMSSW80X/CMSSW_8_0_8_patch1/src/MonoHiggsToGG/analysis/scripts/$srcfile 
+	echo bsub -q 1nh -o $logfile source /afs/cern.ch/user/m/mzientek/private/MonoHgg_Moriond17/CMSSW_8_0_26_patch1/src/MonoHiggsToGG/analysis/scripts/$srcfile 
+	bsub -q 1nh -o $logfile source /afs/cern.ch/user/m/mzientek/private/MonoHgg_Moriond17/CMSSW_8_0_26_patch1/src/MonoHiggsToGG/analysis/scripts/$srcfile 
 	let COUNTER=COUNTER+1 
     fi
 #, for src in list: system(bsub %s -q 2nd, src.replace(log,src)”
--- analysis/scripts/whichFilesMissing.py
+++ analysis/scripts/whichFilesMissing.py
@@ -1,8 +1,18 @@
-import sys
+import sys, os
 
 def AllGlobal(fname):
   fin  = open(fname,"r")
   fout = open(fname+"_log","w")
+
+  path = str(sys.argv[1])
+  name = str(sys.argv[2])
+  indir = []
+  indir = os.listdir(path+"/"+name+"/src/")
+
+  total = 0
+  for files in indir:
+    if files.endswith("src"):
+      total += 1
 
   lines = []
   for line in fin:
@@ -11,9 +21,17 @@ def AllGlobal(fname):
   lines.sort()
   size = len(lines)
   n    = size-1
-  
+ 
+  if lines[0]!=0:
+    fout.write(str(0)+"\n")
+
+  x=lines[n]
+  while x < total:
+    fout.write(str(x)+"\n")
+    x+=1;
+ 
   for i in range(0,n):
-    diff = lines[i+1] - lines[i] - 1 
+    diff = lines[i+1] - lines[i] - 1
     if diff==0:
        continue
     else:
