6703b8b5f103e6029446597f026e28052c627493
Margaret Zientek <mez34@cornell.edu>
2017-05-22 11:04:52 +0200

    make 2d eff plot script, clean fit eff script

--- analysis/work/macros/makeEfficiencyFit.C
+++ analysis/work/macros/makeEfficiencyFit.C
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void	Get1DEfficiency(TFile*, TString, TFile*, TString, Bool_t, TString);
+void	Get1DEfficiency(TFile*, TString, TFile*, TString, Bool_t, Bool_t, TString);
 
 void makeEfficiencyFit()
 {
@@ -22,44 +22,67 @@ void makeEfficiencyFit()
   // ----------------------------------------------------------------
   // Fit w/ deg1 polynomial 
   // ----------------------------------------------------------------
-  Get1DEfficiency(infile,outdir,outfile,"BARY",true,"deg1");
-  Get1DEfficiency(infile,outdir,outfile,"BARY",false,"deg1");
-  Get1DEfficiency(infile,outdir,outfile,"2HDM",true,"deg1"); 
-  Get1DEfficiency(infile,outdir,outfile,"2HDM",false,"deg1"); 
+  //// BARYONIC
+  Get1DEfficiency(infile,outdir,outfile,"BARY",true, true, "deg1"); // fit mDM, highMET
+  Get1DEfficiency(infile,outdir,outfile,"BARY",false,true, "deg1"); // fit mZp, highMET
+  Get1DEfficiency(infile,outdir,outfile,"BARY",true, false,"deg1"); // fit mDM, lowMET
+  Get1DEfficiency(infile,outdir,outfile,"BARY",false,false,"deg1"); // fit mZp, lowMET
+  // 2HDM
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",true, true, "deg1"); // fit mA,  highMET
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",false,true, "deg1"); // fit mZp, highMET 
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",true, false,"deg1"); // fit mA,  lowMET  
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",false,false,"deg1"); // fit mZp, lowMET 
   // ----------------------------------------------------------------
   // Fit w/ deg2 polynomial 
   // ----------------------------------------------------------------
-  Get1DEfficiency(infile,outdir,outfile,"BARY",true,"deg2");
-  Get1DEfficiency(infile,outdir,outfile,"BARY",false,"deg2");
-  Get1DEfficiency(infile,outdir,outfile,"2HDM",true,"deg2"); 
-  Get1DEfficiency(infile,outdir,outfile,"2HDM",false,"deg2"); 
+  // BARYONIC
+  Get1DEfficiency(infile,outdir,outfile,"BARY",true, true, "deg2"); // fit mDM, highMET  
+  Get1DEfficiency(infile,outdir,outfile,"BARY",false,true, "deg2"); // fit mZp, highMET
+  Get1DEfficiency(infile,outdir,outfile,"BARY",true, false,"deg2"); // fit mDM, lowMET
+  Get1DEfficiency(infile,outdir,outfile,"BARY",false,false,"deg2"); // fit mZp, lowMET 
+  // 2HDM                                                                               
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",true, true, "deg2"); // fit mA,  highMET
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",false,true, "deg2"); // fit mZp, highMET  
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",true, false,"deg2"); // fit mA,  lowMET   
+  Get1DEfficiency(infile,outdir,outfile,"2HDM",false,false,"deg2"); // fit mZp, lowMET 
  
+  // ----------------------------------------------------------------
+  // Close the file 
+  // ----------------------------------------------------------------
   outfile->Close();
   delete outfile;
 }
 
-void Get1DEfficiency(TFile* infile, TString outdir, TFile* outfile, TString sig, Bool_t highMET, TString deg)
+void Get1DEfficiency(TFile* infile, TString outdir, TFile* outfile, TString sig, Bool_t useY, Bool_t highMET, TString deg)
 {
+
+  // ----------------------------------------------------------------
+  // Set up variables 
+  // ----------------------------------------------------------------
+  TString met = (highMET)? "HighMET":"LowMET";
+  Int_t model = 0;                                    // BARY
+  if (sig.EqualTo("2HDM",TString::kExact)) model = 1; // 2HDM
+  std::vector< Double_t > mY;
+  if (model==0 && useY)  mY = {1, 10, 50, 150, 500, 1000};                         // BARY DM
+  if (model==1 && useY)  mY = {300, 400, 500, 600, 700, 800};                      // 2HDM A
+  if (model==0 && !useY) mY = {10, 20, 50, 100, 200, 300, 500, 1000, 2000, 10000}; // BARY Zp
+  if (model==1 && !useY) mY = {600, 800, 1000, 1200, 1400, 1700, 2000, 2500};      // 2HDM Zp
+  TString xname = "DM";
+  if (model==1 && useY) xname = "A";
+  if (!useY)            xname = "Zp";
+  Double_t minX = 0;
+  Double_t maxX = 0;
+  if (model==0 && useY){  minX = 0;   maxX = 2500; }
+  if (model==1 && useY){  minX = 500; maxX = 3000; }
+  if (model==0 && !useY){ minX = 0;   maxX = 1050; }
+  if (model==1 && !useY){ minX = 250; maxX = 850;  }
 
   // ----------------------------------------------------------------
   // Get the 2D efficiencies 
   // ----------------------------------------------------------------
-  TString met = (highMET)? "HighMET":"LowMET";
   TString histoname = Form("h_%s_Eff_%s",sig.Data(),met.Data());
   TH2D* h_eff = (TH2D*)infile->Get(Form("%s",histoname.Data()));
   TH2D* h_err = (TH2D*)infile->Get(Form("%s_err",histoname.Data()));
-
-  // ----------------------------------------------------------------
-  // Make 1D efficiencies for mChi or mA0 (Y axis in 2D) 
-  // ----------------------------------------------------------------
-  std::vector< Double_t > mY = {1, 10, 50, 150, 500, 1000};                     // BARY DM
-  if (sig.EqualTo("2HDM",TString::kExact)) mY = {300, 400, 500, 600, 700, 800}; // 2HDM A0
-  TString xname = "DM";
-  if (sig.EqualTo("2HDM",TString::kExact)) xname = "A";
-  Double_t minX = 0;                                                            // BARY min Zp
-  Double_t maxX = 2500;                                                         // BARY max Zp
-  //Double_t maxX = 10005;                                                        // BARY max Zp
-  if (sig.EqualTo("2HDM",TString::kExact)){ minX = 500; maxX = 3000; }          // 2HDM Zp
 
   // ----------------------------------------------------------------
   // Set up the fit 
@@ -67,23 +90,38 @@ void Get1DEfficiency(TFile* infile, TString outdir, TFile* outfile, TString sig,
   TString eqn = "[0] + [1]*x";
   if (deg.EqualTo("deg2",TString::kExact)) eqn = "[0] + [1]*x + [2]*x^2";
   TF1 *polN = new TF1("polN",Form("%s",eqn.Data()),-20,20);
-  TH1D* h_prob = new TH1D("h_prob",Form("h_prob_%s_%s_%s",deg.Data(),sig.Data(),met.Data()),mY.size(),0,mY.size()); // histo w/ prob for each fit
+  TString pname = Form("h_prob_%s_%s_%s_m%s",deg.Data(),sig.Data(),met.Data(),xname.Data());
+  TH1D* h_prob = new TH1D(pname,pname,mY.size(),0,mY.size()); // histo w/ prob for each fit
 
   outfile->cd();
   TCanvas *c = new TCanvas();
 
   for (UInt_t m=0; m < mY.size(); m++){
-    TH1D* h_eff_1D = new TH1D(Form("h_eff_1D_m%1.f",mY[m]),Form("h_eff_1D_%s_%s_%s_m%1.f",deg.Data(),sig.Data(),met.Data(),mY[m]),(maxX-minX),minX,maxX);
-    h_eff_1D = h_eff->ProjectionX(Form("h_eff_1D_%s_%s_%s_m%1.f",deg.Data(),sig.Data(),met.Data(),mY[m]),mY[m]-1,mY[m]+1); 
+    // ----------------------------------------------------------------
+    // Get Efficiencies (projection from 2D plot) 
+    // ----------------------------------------------------------------
+    TString hname = Form("h_eff_1D_%s_%s_%s_m%s%1.f",deg.Data(),sig.Data(),met.Data(),xname.Data(),mY[m]);
+    TH1D* h_eff_1D = new TH1D(hname,hname,(maxX-minX),minX,maxX);
+    if (useY) h_eff_1D = h_eff->ProjectionX(hname,mY[m]-1,mY[m]+1); 
+    else      h_eff_1D = h_eff->ProjectionY(hname,mY[m]-1,mY[m]+1); 
     h_eff_1D->GetXaxis()->SetRange(minX,maxX);
 
     // ----------------------------------------------------------------
     // Get errors on efficiencies 
     // ----------------------------------------------------------------
-    Double_t binY = h_err->GetYaxis()->FindBin(mY[m]);
-    for (UInt_t binX=0; binX < h_eff_1D->GetSize(); binX++){
-      Double_t errX = h_err->GetBinContent(binX,binY);
-      h_eff_1D->SetBinError(binX,errX);
+    if (useY){ 
+      Double_t binY = h_err->GetYaxis()->FindBin(mY[m]);
+      for (UInt_t binX=0; binX < h_eff_1D->GetSize(); binX++){
+        Double_t errX = h_err->GetBinContent(binX,binY);
+        h_eff_1D->SetBinError(binX,errX);
+      } 
+    }
+    else{
+      Double_t binX = h_err->GetXaxis()->FindBin(mY[m]);
+      for (UInt_t binY=0; binY < h_eff_1D->GetSize(); binY++){
+        Double_t errY = h_err->GetBinContent(binX,binY);
+        h_eff_1D->SetBinError(binY,errY);
+      }
     }
 
     // ----------------------------------------------------------------
@@ -103,18 +141,18 @@ void Get1DEfficiency(TFile* infile, TString outdir, TFile* outfile, TString sig,
     h_eff_1D->SetMarkerSize(0.5);
     h_eff_1D->Draw("PE");
     h_eff_1D->Write();
-    gStyle->SetStatX(0.85);
-    gStyle->SetStatY(0.5);
+    gStyle->SetStatX(0.89);
+    gStyle->SetStatY(0.89);
     c->SetLogx(0);
     c->SaveAs(Form("%sfit_%s/%s_m%s/h_1Deff_%s_%s_%s_m%1.f_lin.png",outdir.Data(),deg.Data(),sig.Data(),xname.Data(),deg.Data(),sig.Data(),met.Data(),mY[m]));
     c->SaveAs(Form("%sfit_%s/%s_m%s/h_1Deff_%s_%s_%s_m%1.f_lin.pdf",outdir.Data(),deg.Data(),sig.Data(),xname.Data(),deg.Data(),sig.Data(),met.Data(),mY[m]));
     c->SetLogx(1);
-    TPaveStats* stat = (TPaveStats*)h_eff_1D->GetListOfFunctions()->FindObject("stats");
-    stat->SetX1NDC(0.15);
-    stat->SetX2NDC(0.40);
-    stat->SetY1NDC(0.80);
-    c->Modified();
-    c->Update(); 
+    //TPaveStats* stat = (TPaveStats*)h_eff_1D->GetListOfFunctions()->FindObject("stats");
+    //stat->SetX1NDC(0.15);
+    //stat->SetX2NDC(0.40);
+    //stat->SetY1NDC(0.80);
+    //c->Modified();
+    //c->Update(); 
     c->SaveAs(Form("%sfit_%s/%s_m%s/h_1Deff_%s_%s_%s_m%1.f_log.png",outdir.Data(),deg.Data(),sig.Data(),xname.Data(),deg.Data(),sig.Data(),met.Data(),mY[m]));
     c->SaveAs(Form("%sfit_%s/%s_m%s/h_1Deff_%s_%s_%s_m%1.f_log.pdf",outdir.Data(),deg.Data(),sig.Data(),xname.Data(),deg.Data(),sig.Data(),met.Data(),mY[m]));
     delete h_eff_1D; 
@@ -127,13 +165,16 @@ void Get1DEfficiency(TFile* infile, TString outdir, TFile* outfile, TString sig,
   c->SetLogx(0);
   gStyle->SetOptStat(0);
   h_prob->GetYaxis()->SetTitle("Prob(#chi^{2})");
-  h_prob->GetXaxis()->SetTitle("m_{#chi}");
-  if (sig.EqualTo("2HDM",TString::kExact)) h_prob->GetXaxis()->SetTitle("m_{A}");
+  if (model==0 && useY)  h_prob->GetXaxis()->SetTitle("m_{#chi}");
+  if (model==1 && useY)  h_prob->GetXaxis()->SetTitle("m_{A}");
+  if (model==0 && !useY) h_prob->GetXaxis()->SetTitle("m_{Z'}");
+  if (model==1 && !useY) h_prob->GetXaxis()->SetTitle("m_{Z'}");
   h_prob->Draw("HIST");
   h_prob->Write();
   c->SaveAs(Form("%sfit_%s/%s_m%s/h_fitprob_%s_%s_%s.png",outdir.Data(),deg.Data(),sig.Data(),xname.Data(),deg.Data(),sig.Data(),met.Data()));
   c->SaveAs(Form("%sfit_%s/%s_m%s/h_fitprob_%s_%s_%s.pdf",outdir.Data(),deg.Data(),sig.Data(),xname.Data(),deg.Data(),sig.Data(),met.Data()));
- 
+   
+  delete h_prob; 
   delete c;
 
 }
