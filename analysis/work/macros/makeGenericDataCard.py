d4aba8bac11d69dbce43ab5fb2ac2a0ded002c95
Margaret Zientek <mez34@cornell.edu>
2017-07-27 16:07:30 +0200

    allow scripts to take 2HDM input -- just need new eff. function

--- analysis/work/macros/makeGenericDataCard.py
+++ analysis/work/macros/makeGenericDataCard.py
@@ -9,7 +9,7 @@ from ROOT import RooDataSet
 from ROOT import RooDataHist
 from ROOT import RooAbsReal
 
-def getEff(cat,mZp,mDM):
+def getEffBary(cat,mZp,mDM):
   eff = 1.0
   if cat==1: #highMET
     eff=(0.00474*mZp+0.0397*mDM-1.4e-07*mZp*mZp-1.19e-05*mDM*mDM-2.95e-06*mZp*mDM+6.1583)
@@ -17,12 +17,26 @@ def getEff(cat,mZp,mDM):
     eff=(-3e-04*mZp-4e-03*mDM-1.3e-08*mZp*mZp-4.4e-07*mDM*mDM-3.8e-07*mZp*mDM+6.5448)
   return eff
 
-def makeCard(mZp,mChi,DIR):
+def getEff2HDM(cat,mZp,mDM):
+  eff = 1.0
+  if cat==1: #highMET
+    eff = 1.0
+  if cat==0: #lowMET
+    eff = 1.0
+  return eff
+
+def makeCard(TYPE,mZp,mChi,DIR):
 
   # Setup input and output files
-  indir = DIR 
-  old_str = "sig_ZpBaryonic_mZP10_mChi1"
-  new_str = "sig_ZpBaryonic_mZP"+mZp+"_mChi"+mChi
+  indir = DIR
+  old_str = ""
+  new_str = "" 
+  if (TYPE=="BARY"):
+    old_str = "sig_ZpBaryonic_mZP10_mChi1"
+    new_str = "sig_ZpBaryonic_mZP"+mZp+"_mChi"+mChi
+  if (TYPE=="2HDM"):
+    old_str = "sig_2HDM_mZP600_mA0300"
+    new_str = "sig_2HDM_mZP"+mZp+"_mA0"+mChi
   fin  = open(indir+"dataCard_"+old_str+"_13TeV.txt","r")
   fout = open(indir+"dataCard_"+new_str+"_13TeV.txt","w") 
   rin  = ROOT.TFile(indir+old_str+"_13TeV.root") 
@@ -37,10 +51,20 @@ def makeCard(mZp,mChi,DIR):
   # Get the old and new eff
   mZ  = float(mZp)
   mDM = float(mChi)
-  old_efflowMET  = getEff(0,10,1)
-  old_effhighMET = getEff(1,10,1)
-  new_efflowMET  = getEff(0,mZ,mDM)
-  new_effhighMET = getEff(1,mZ,mDM)
+  old_efflowMET  = 1.0
+  old_effhighMET = 1.0
+  new_efflowMET  = 1.0
+  new_effhighMET = 1.0
+  if (TYPE=="BARY"):
+    old_efflowMET  = getEffBary(0,10,1)
+    old_effhighMET = getEffBary(1,10,1)
+    new_efflowMET  = getEffBary(0,mZ,mDM)
+    new_effhighMET = getEffBary(1,mZ,mDM)
+  if (TYPE=="2HDM"):
+    old_efflowMET  = getEff2HDM(0,600,300)
+    old_effhighMET = getEff2HDM(1,600,300)
+    new_efflowMET  = getEff2HDM(0,mZ,mDM)
+    new_effhighMET = getEff2HDM(1,mZ,mDM)
   scale_lowMET   = new_efflowMET/old_efflowMET
   scale_highMET  = new_effhighMET/old_effhighMET
   #print("Old eff: low = %f, high = %f" %(old_efflowMET,old_effhighMET)) 
@@ -111,17 +135,22 @@ if __name__ == "__main__":
   filename = ""
   DIR = ""
 
-  if len(sys.argv) < 3: 
-    print("Invalid usage! Correct usage is: python makeGenericDataCard.py [mZp] [mChi] [DIR]")
+  if len(sys.argv) < 4: 
+    print("Invalid usage! Correct usage is: python makeGenericDataCard.py [mZp] [mChi] [DIR] [type]")
     sys.exit()
   else:
-    MZP = sys.argv[1]
+    MZP  = sys.argv[1]
     MCHI = sys.argv[2]
-    DIR = sys.argv[3]
-  if MZP=="10" and MCHI=="1": 
-    print("Can't use mZp = 10 and mChi = 1. That is the input! Can't be the output!")
+    DIR  = sys.argv[3]
+    TYPE = sys.argv[4] 
+  if TYPE=="BARY" and MZP=="10" and MCHI=="1": 
+    print("Bary: Can't use mZp = 10 and mChi = 1. That is the input! Can't be the output!")
     sys.exit()
+  if TYPE=="2HDM" and MZP=="600" and MCHI=="300":
+    print("2HDM: Can't use mZp = 600 and mA = 300. That is the input! Can't be the output!")
+    sys.exit()
+    
   
-  print("Making datacard for mZp = %s , mChi = %s" %(MZP, MCHI))
-  makeCard(MZP,MCHI,DIR)
+  print("Making datacard (%s) for mZp = %s , mChi = %s" %(TYPE, MZP, MCHI))
+  makeCard(TYPE,MZP,MCHI,DIR)
   print("Finished")
--- analysis/work/macros/runMakeGenericDataCard.sh
+++ analysis/work/macros/runMakeGenericDataCard.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-dir="ntuples4fit_new/"
+dir="ntuples4fit_mDM1/"
+type="BARY"
 
-for mZp in "10"
+for mDM in "1"
   do 
-  for mDM in "10" "25" "50" "75" "100" "125" "150" "175" "200" "225" "250" "275" "300" "350" "400" "450" "500" "550" "600" "650" "700" "750" "800" "850" "900" "950" "1000" "1050" "1100"
+  for mZp in "25" "50" "75" "100" "125" "150" "175" "200" "225" "250" "275" "300" "350" "400" "450" "500" "550" "600" "650" "700" "750" "800" "850" "900" "950" "1000" "1050" "1100"
     do 
-    python makeGenericDataCard.py $mZp $mDM ${dir} 
+    python makeGenericDataCard.py $mZp $mDM ${dir} ${type} 
     done
   done
 
